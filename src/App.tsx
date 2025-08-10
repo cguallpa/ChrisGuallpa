be08fd47ef769a97b8d2b31265a27bf125018773
cguallpa <christopherguallpa@outlook.com>
2025-08-09 20:40:34 -0400

    route homepage for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />}></Route>
+          <Route path="/ChrisGuallpa" element={<Home />}></Route>
+          {/* Add other routes here */}
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </BrowserRouter>
